85745fac0ae1181b603ac1c06c2217b9e2998251
brnoliveira110 <brnoliveirasantana@gmail.com>
2022-02-20 23:42:56 -0300

    Best commit

---
+++ src/views/Contato.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="contato">
+    <h1>Contato</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Contato",
+  components: {},
+};
+</script>
---
+++ src/views/Cursos.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="cursos">
+    <h1>Cursos</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Cursos",
+  components: {},
+};
+</script>
